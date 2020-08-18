491e704dcc338476ef9fc44630dac025249fb0d1
Jing <429712282@qq.com>
2020-08-18 15:44:01 +0800

    sisth commit

--- src/main/java/com/example/data_structure/Controller/DataController.java
+++ src/main/java/com/example/data_structure/Controller/DataController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -120,6 +121,13 @@ public class DataController {
             return "usermanage";
         }
     }
+    /*
+    @RequestMapping("/index_cont")
+    public ModelAndView firstpage(){
+        return new ModelAndView("index_cont");
+    }
 
+
+     */
 
 }
